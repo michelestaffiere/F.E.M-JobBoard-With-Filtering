f95566ca17b65b6aef42843cb898d7ab2a28abe5
MicheleStaffiere <michelestaffiere1@gmail.com>
2023-05-24 14:41:43 -0400

    JS code to append and display all jobs and their tags completed. still todo is filtering and eventlisteners for tags

--- scripts/job-data.js
+++ scripts/job-data.js
@@ -5,7 +5,14 @@ const jobsToDisplay =[
        location: `USA only`,
        postDate: `1d ago`,
        type: `Full Time`,
-       companyImg: `<img src="./styles/assets/photosnap.svg" alt="Photosnap logo" srcset="">`
+       companyImg: `<img src="./styles/assets/photosnap.svg" alt="Photosnap logo" srcset="">`,
+       tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Senior`,
+         tag3: `HTML`,
+         tag4: `CSS`,
+         tag5: `Javascript`
+       }
 
     },
     {
@@ -14,7 +21,13 @@ const jobsToDisplay =[
         location: `Remote`,
         type: `Part Time`,
         postDate: `1d ago`,
-        companyImg: `<img src="./styles/assets/manage.svg" alt="Manage logo" srcset="">`
+        companyImg: `<img src="./styles/assets/manage.svg" alt="Manage logo" srcset="">`,
+        tagsObj : {
+         tag1: `Fullstack`,
+         tag2: `Midweight`,
+         tag3: `Python`,
+         tag4: `React`,
+       }
      },
      {
         employer: `Account`,
@@ -22,15 +35,28 @@ const jobsToDisplay =[
         location: `USA only`,
         type: `Part Time`,
         postDate:`2d ago`,
-        companyImg: `<img src="./styles/assets/account.svg" alt="Account logo" srcset="">`
+        companyImg: `<img src="./styles/assets/account.svg" alt="Account logo" srcset="">`,
+        tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Junior`,
+         tag3: `React`,
+         tag4: `SaSS`,
+         tag5: `Javascript`
+       }
      },
      {
         employer: `MyHome`,
-        title: `Senior Frontend Developer`,
+        title: `Junior Frontend Developer`,
         location: `USA only`,
         type: `Full Time`,
         postDate: `5d ago`,
-        companyImg: `<img src="./styles/assets/myhome.svg" alt="MyHome logo" srcset="">`
+        companyImg: `<img src="./styles/assets/myhome.svg" alt="MyHome logo" srcset="">`,
+        tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Junior`,
+         tag3: `CSS`,
+         tag4: `Javascript`
+       }
      },
      {
         employer: `Loop Studios`,
@@ -38,7 +64,14 @@ const jobsToDisplay =[
         location: `Worldwide`,
         type: `Full Time`,
         postDate:`1w ago`,
-        companyImg: `<img src="./styles/assets/loop-studios.svg" alt="Loop Studios logo" srcset="">`
+        companyImg: `<img src="./styles/assets/loop-studios.svg" alt="Loop Studios logo" srcset="">`,
+        tagsObj : {
+         tag1: `Fullstack`,
+         tag2: `Midweight`,
+         tag3: `Javascript`,
+         tag4: `SaSS`,
+         tag5: `Ruby`
+       }
      },
      {
         employer: `FaceIt`,
@@ -46,7 +79,13 @@ const jobsToDisplay =[
         location: `Uk only`,
         type: `Full Time`,
         postDate:`2w ago`,
-        companyImg: `<img src="./styles/assets/faecit.svg" alt="Faceit logo" srcset="">`
+        companyImg: `<img src="./styles/assets/faceit.svg" alt="Faceit logo" srcset="">`,
+        tagsObj : {
+         tag1: `Backend`,
+         tag2: `Junior`,
+         tag3: `Ruby`,
+         tag4: `RoR`
+       }
      },
      {
         employer: `Shortly`,
@@ -54,7 +93,14 @@ const jobsToDisplay =[
         location: `Worldwide`,
         type: `Full Time`,
         postDate:`2w ago`,
-        companyImg: `<img src="./styles/assets/shortly.svg" alt="Shortly logo" srcset="">`
+        companyImg: `<img src="./styles/assets/shortly.svg" alt="Shortly logo" srcset="">`,
+        tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Junior`,
+         tag3: `HTML`,
+         tag4: `SaSS`,
+         tag5: `Javascript`
+       }
      },
      {
         employer: `Insure`,
@@ -62,7 +108,14 @@ const jobsToDisplay =[
         location: `USA only`,
         type: `Full Time`,
         postDate:`2w ago`,
-        companyImg: `<img src="./styles/assets/insure.svg" alt="Insure logo" srcset="">`
+        companyImg: `<img src="./styles/assets/insure.svg" alt="Insure logo" srcset="">`,
+        tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Junior`,
+         tag3: `Vue`,
+         tag4: `Javascript`,
+         tag5: `SaSS`
+       }
      },
      {
         employer: `Eyecam Co.`,
@@ -70,7 +123,14 @@ const jobsToDisplay =[
         location: `Worldwide`,
         type: `Full Time`,
         postDate:`3w ago`,
-        companyImg: `<img src="./styles/assets/eyecam-co.svg" alt="Eyecam logo" srcset="">`
+        companyImg: `<img src="./styles/assets/eyecam-co.svg" alt="Eyecam logo" srcset="">`,
+        tagsObj : {
+         tag1: `Fullstack`,
+         tag2: `Midweight`,
+         tag3: `Javascript`,
+         tag4: `Django`,
+         tag5: `Python`
+       }
      },
      {
         employer: `The Air Filter Company`,
@@ -78,28 +138,59 @@ const jobsToDisplay =[
         location: `Canada only`,
         type: `Part Time`,
         postDate:`1mo ago`,
-        companyImg: ` <img src="./styles/assets/the-air-filter-company.svg" alt="The Air Filter Company logo" srcset="">`
+        companyImg: ` <img src="./styles/assets/the-air-filter-company.svg" alt="The Air Filter Company logo" srcset="">`,
+        tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Junior`,
+         tag3: `React`,
+         tag4: `SaSS`,
+         tag5: `Javascript`
+       }
      },
-]
-P
+];
 const JobContainer = document.querySelector('.job-list');
 let jobListHTML = '';
+let postCounter = 1;
 
 jobsToDisplay.forEach((job) => {
+//   let jobPost = 'posting' + postCounter;${jobPost}
   jobListHTML +=
-    `<li>
+    `<li class = "job-posting">
        ${job.companyImg}
         <div class="posting-information">
             <p>${job.employer}</p>
             <h3>${job.title}</h3>
-            <ul>
+            <ul class="date-locale">
                 <li>${job.postDate}</li>
                 <li>${job.type}</li>
                 <li>${job.location}</li>
             </ul>
         </div>
     </li>`;
-  console.log(`Appended ${job.employer}'s posting.`);
+  postCounter++;
+});
+JobContainer.innerHTML = jobListHTML;
+
+// forEach Loop responsible for appending tags to each listing.:
+const allJobPostings = document.querySelectorAll(`.job-posting`);
+let appendCounter = 0;
+jobsToDisplay.forEach((job) =>{
+   const jobTags = Object.entries(job.tagsObj);
+   console.log(allJobPostings[appendCounter]);
+   console.log(jobTags);
+   jobTagsUl = document.createElement(`ul`);
+   jobTagsUl.classList =`tags`;
+   allJobPostings[appendCounter].append(jobTagsUl);
+   console.log(jobTagsUl);
+
+   jobTags.forEach(([tag, value]) =>{
+
+      let tagsLi = document.createElement(`li`);
+      let tagValue = value;
+      tagsLi.textContent = tagValue;
+      jobTagsUl.append(tagsLi);
+      // TODO addevenlistener for tag filtering     
+   });
+   appendCounter = appendCounter + 1;
 });
 
-JobContainer.innerHTML = jobListHTML;
