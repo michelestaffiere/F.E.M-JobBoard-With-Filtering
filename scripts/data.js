02e9a00536055fe590c71219a45fd415832712f0
MicheleStaffiere <michelestaffiere1@gmail.com>
2023-05-30 15:51:10 -0400

    refactored how tags get added to their taglist parent, laid out ground work for how the jobs will be rendered to the page on first load

---
+++ scripts/data.js
@@ -0,0 +1,170 @@
+const jobsToDisplay =[
+    {
+       employer: `Photosnap`,
+       title: `Senior Frontend Developer`,
+       location: `USA only`,
+       postDate: `1d ago`,
+       type: `Full Time`,
+       companyImg: `<img src="./styles/assets/photosnap.svg" alt="Photosnap logo" srcset="">`,
+       isFeatured:true,
+       isNew:true,
+       tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Senior`,
+         tag3: `HTML`,
+         tag4: `CSS`,
+         tag5: `Javascript`
+       }
+
+    },
+    {
+        employer: `Manage`,
+        title: `Fullstack Developer`,
+        location: `Remote`,
+        type: `Part Time`,
+        postDate: `1d ago`,
+        companyImg: `<img src="./styles/assets/manage.svg" alt="Manage logo" srcset="">`,
+        isFeatured:true,
+        isNew:true,
+        tagsObj : {
+         tag1: `Fullstack`,
+         tag2: `Midweight`,
+         tag3: `Python`,
+         tag4: `React`,
+       }
+     },
+     {
+        employer: `Account`,
+        title: `Junior Frontend Developer`,
+        location: `USA only`,
+        type: `Part Time`,
+        postDate:`2d ago`,
+        companyImg: `<img src="./styles/assets/account.svg" alt="Account logo" srcset="">`,
+        isFeatured:false,
+        isNew:true,
+        tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Junior`,
+         tag3: `React`,
+         tag4: `SaSS`,
+         tag5: `Javascript`
+       }
+     },
+     {
+        employer: `MyHome`,
+        title: `Junior Frontend Developer`,
+        location: `USA only`,
+        type: `Full Time`,
+        postDate: `5d ago`,
+        companyImg: `<img src="./styles/assets/myhome.svg" alt="MyHome logo" srcset="">`,
+        isFeatured:false,
+        isNew:false,
+        tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Junior`,
+         tag3: `CSS`,
+         tag4: `Javascript`
+       }
+     },
+     {
+        employer: `Loop Studios`,
+        title: `Software Engineer`,
+        location: `Worldwide`,
+        type: `Full Time`,
+        postDate:`1w ago`,
+        companyImg: `<img src="./styles/assets/loop-studios.svg" alt="Loop Studios logo" srcset="">`,
+        isFeatured:false,
+        tagsObj : {
+         tag1: `Fullstack`,
+         tag2: `Midweight`,
+         tag3: `Javascript`,
+         tag4: `SaSS`,
+         tag5: `Ruby`
+       }
+     },
+     {
+        employer: `FaceIt`,
+        title: `Junior Backend Developer`,
+        location: `Uk only`,
+        type: `Full Time`,
+        postDate:`2w ago`,
+        companyImg: `<img src="./styles/assets/faceit.svg" alt="Faceit logo" srcset="">`,
+        isFeatured:false,
+        isNew:false,
+        tagsObj : {
+         tag1: `Backend`,
+         tag2: `Junior`,
+         tag3: `Ruby`,
+         tag4: `RoR`
+       }
+     },
+     {
+        employer: `Shortly`,
+        title: `Junior Developer`,
+        location: `Worldwide`,
+        type: `Full Time`,
+        postDate:`2w ago`,
+        companyImg: `<img src="./styles/assets/shortly.svg" alt="Shortly logo" srcset="">`,
+        isFeatured:false,
+        isNew:false,
+        tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Junior`,
+         tag3: `HTML`,
+         tag4: `SaSS`,
+         tag5: `Javascript`
+       }
+     },
+     {
+        employer: `Insure`,
+        title: `Junior Frontend Developer`,
+        location: `USA only`,
+        type: `Full Time`,
+        postDate:`2w ago`,
+        companyImg: `<img src="./styles/assets/insure.svg" alt="Insure logo" srcset="">`,
+        isFeatured:false,
+        isNew:false,
+        tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Junior`,
+         tag3: `Vue`,
+         tag4: `Javascript`,
+         tag5: `SaSS`
+       }
+     },
+     {
+        employer: `Eyecam Co.`,
+        title: `Full Stack Developer`,
+        location: `Worldwide`,
+        type: `Full Time`,
+        postDate:`3w ago`,
+        companyImg: `<img src="./styles/assets/eyecam-co.svg" alt="Eyecam logo" srcset="">`,
+        isFeatured:false,
+        isNew:false,
+        tagsObj : {
+         tag1: `Fullstack`,
+         tag2: `Midweight`,
+         tag3: `Javascript`,
+         tag4: `Django`,
+         tag5: `Python`
+       }
+     },
+     {
+        employer: `The Air Filter Company`,
+        title: `Front-end Dev`,
+        location: `Canada only`,
+        type: `Part Time`,
+        postDate:`1mo ago`,
+        companyImg: ` <img src="./styles/assets/the-air-filter-company.svg" alt="The Air Filter Company logo" srcset="">`,
+        isFeatured:false,
+        isNew:false,
+        tagsObj : {
+         tag1: `Frontend`,
+         tag2: `Junior`,
+         tag3: `React`,
+         tag4: `SaSS`,
+         tag5: `Javascript`
+        }
+    },
+];
+export default jobsToDisplay;
--- scripts/job-data.js
+++ scripts/job-data.js
@@ -1,219 +1,76 @@
-const jobsToDisplay =[
-    {
-       employer: `Photosnap`,
-       title: `Senior Frontend Developer`,
-       location: `USA only`,
-       postDate: `1d ago`,
-       type: `Full Time`,
-       companyImg: `<img src="./styles/assets/photosnap.svg" alt="Photosnap logo" srcset="">`,
-       isFeatured:true,
-       isNew:true,
-       tagsObj : {
-         tag1: `Frontend`,
-         tag2: `Senior`,
-         tag3: `HTML`,
-         tag4: `CSS`,
-         tag5: `Javascript`
-       }
+import jobsToDisplay from "./data.js"
 
-    },
-    {
-        employer: `Manage`,
-        title: `Fullstack Developer`,
-        location: `Remote`,
-        type: `Part Time`,
-        postDate: `1d ago`,
-        companyImg: `<img src="./styles/assets/manage.svg" alt="Manage logo" srcset="">`,
-        isFeatured:true,
-        isNew:true,
-        tagsObj : {
-         tag1: `Fullstack`,
-         tag2: `Midweight`,
-         tag3: `Python`,
-         tag4: `React`,
-       }
-     },
-     {
-        employer: `Account`,
-        title: `Junior Frontend Developer`,
-        location: `USA only`,
-        type: `Part Time`,
-        postDate:`2d ago`,
-        companyImg: `<img src="./styles/assets/account.svg" alt="Account logo" srcset="">`,
-        isFeatured:false,
-        isNew:true,
-        tagsObj : {
-         tag1: `Frontend`,
-         tag2: `Junior`,
-         tag3: `React`,
-         tag4: `SaSS`,
-         tag5: `Javascript`
-       }
-     },
-     {
-        employer: `MyHome`,
-        title: `Junior Frontend Developer`,
-        location: `USA only`,
-        type: `Full Time`,
-        postDate: `5d ago`,
-        companyImg: `<img src="./styles/assets/myhome.svg" alt="MyHome logo" srcset="">`,
-        isFeatured:false,
-        isNew:false,
-        tagsObj : {
-         tag1: `Frontend`,
-         tag2: `Junior`,
-         tag3: `CSS`,
-         tag4: `Javascript`
-       }
-     },
-     {
-        employer: `Loop Studios`,
-        title: `Software Engineer`,
-        location: `Worldwide`,
-        type: `Full Time`,
-        postDate:`1w ago`,
-        companyImg: `<img src="./styles/assets/loop-studios.svg" alt="Loop Studios logo" srcset="">`,
-        isFeatured:false,
-        tagsObj : {
-         tag1: `Fullstack`,
-         tag2: `Midweight`,
-         tag3: `Javascript`,
-         tag4: `SaSS`,
-         tag5: `Ruby`
-       }
-     },
-     {
-        employer: `FaceIt`,
-        title: `Junior Backend Developer`,
-        location: `Uk only`,
-        type: `Full Time`,
-        postDate:`2w ago`,
-        companyImg: `<img src="./styles/assets/faceit.svg" alt="Faceit logo" srcset="">`,
-        isFeatured:false,
-        isNew:false,
-        tagsObj : {
-         tag1: `Backend`,
-         tag2: `Junior`,
-         tag3: `Ruby`,
-         tag4: `RoR`
-       }
-     },
-     {
-        employer: `Shortly`,
-        title: `Junior Developer`,
-        location: `Worldwide`,
-        type: `Full Time`,
-        postDate:`2w ago`,
-        companyImg: `<img src="./styles/assets/shortly.svg" alt="Shortly logo" srcset="">`,
-        isFeatured:false,
-        isNew:false,
-        tagsObj : {
-         tag1: `Frontend`,
-         tag2: `Junior`,
-         tag3: `HTML`,
-         tag4: `SaSS`,
-         tag5: `Javascript`
-       }
-     },
-     {
-        employer: `Insure`,
-        title: `Junior Frontend Developer`,
-        location: `USA only`,
-        type: `Full Time`,
-        postDate:`2w ago`,
-        companyImg: `<img src="./styles/assets/insure.svg" alt="Insure logo" srcset="">`,
-        isFeatured:false,
-        isNew:false,
-        tagsObj : {
-         tag1: `Frontend`,
-         tag2: `Junior`,
-         tag3: `Vue`,
-         tag4: `Javascript`,
-         tag5: `SaSS`
-       }
-     },
-     {
-        employer: `Eyecam Co.`,
-        title: `Full Stack Developer`,
-        location: `Worldwide`,
-        type: `Full Time`,
-        postDate:`3w ago`,
-        companyImg: `<img src="./styles/assets/eyecam-co.svg" alt="Eyecam logo" srcset="">`,
-        isFeatured:false,
-        isNew:false,
-        tagsObj : {
-         tag1: `Fullstack`,
-         tag2: `Midweight`,
-         tag3: `Javascript`,
-         tag4: `Django`,
-         tag5: `Python`
-       }
-     },
-     {
-        employer: `The Air Filter Company`,
-        title: `Front-end Dev`,
-        location: `Canada only`,
-        type: `Part Time`,
-        postDate:`1mo ago`,
-        companyImg: ` <img src="./styles/assets/the-air-filter-company.svg" alt="The Air Filter Company logo" srcset="">`,
-        isFeatured:false,
-        isNew:false,
-        tagsObj : {
-         tag1: `Frontend`,
-         tag2: `Junior`,
-         tag3: `React`,
-         tag4: `SaSS`,
-         tag5: `Javascript`
-       }
-     },
-];
+// const render = () =>{
+
+// };
 const JobContainer = document.querySelector('.job-list');
+const filterSection = document.querySelector('.filtered-section');
+const filteredTagsArray = []
+
 let jobListHTML = '';
 let postCounter = 1;
-
 jobsToDisplay.forEach((job) => {
-//   let jobPost = 'posting' + postCounter;${jobPost}
+  //   let jobPost = 'posting' + postCounter;${jobPost}
   jobListHTML +=
-    `<li class = "job-posting">
-       ${job.companyImg}
-        <div class="posting-information">
-            <p>${job.employer}</p>
-            <h3>${job.title}</h3>
-            <ul class="date-locale">
-                <li>${job.postDate}</li>
-                <li>${job.type}</li>
-                <li>${job.location}</li>
-            </ul>
-        </div>
-    </li>`;
+  `<li class = "job-posting">
+  ${job.companyImg}
+  <div class="posting-information">
+  <p>${job.employer}</p>
+  <h3>${job.title}</h3>
+  <ul class="date-locale">
+  <li>${job.postDate}</li>
+  <li>${job.type}</li>
+  <li>${job.location}</li>
+  </ul>
+  </div>
+  </li>`;
   postCounter++;
 });
 JobContainer.innerHTML = jobListHTML;
 
-
-// forEach Loop responsible for appending tags to each listing.:
+// declaring after because doesnt exist until render is ran at least once.
 const allJobPostings = document.querySelectorAll(`.job-posting`);
+
 let appendCounter = 0;
 jobsToDisplay.forEach((job) =>{
 
-   const jobTags = Object.entries(job.tagsObj);//Object.entries() outputs an array of arrays, with each inner array having two elements (key value pair)
-   //console.log(allJobPostings[appendCounter]);  using square brackets because using a varaible for array index.
-   jobTagsUl = document.createElement(`ul`);
+   const jobTags = Object.values(job.tagsObj);//Object.entries() outputs an array of arrays, with each inner array having two elements (key value pair)
+   const jobTagsUl = document.createElement(`ul`);
    jobTagsUl.classList =`tagParent`;
-   allJobPostings[appendCounter].append(jobTagsUl);
+   allJobPostings[appendCounter].appendChild(jobTagsUl);
 
-   jobTags.forEach(([tag, value]) =>{
 
+   jobTags.forEach((tag) =>{
       let tagsLi = document.createElement(`li`);
       tagsLi.className = "tagChild";
-      let tagValue = value;
+      let tagValue = tag;
       tagsLi.textContent = tagValue;
       jobTagsUl.append(tagsLi);   
    });
    appendCounter++;
 });
-// creating an array of all tags. (might not be needed?)
-const allTags = document.querySelectorAll(`.tagChild`);
+
+// make a function called jobListingMatches Filter
+// takes in filtersArray, and jobListing. 
+// displays jobs with matching filters.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// forEach Loop responsible for appending tags to each listing.:
 
 //move the arrow function code here after finished prototyping.
 //const filterPosts (tag) =>{};
@@ -221,11 +78,11 @@ const allTags = document.querySelectorAll(`.tagChild`);
 
 
 // Select the filtered section element
-const filterSection = document.querySelector('.filtered-section');
-const filteredTagsArray = []
+
 // Iterate over each job posting element in the allJobPostings array
 allJobPostings.forEach((posting) => {
   // Add a click event listener to each job posting element
+
   posting.addEventListener('click', (e) => {
     // Check if the clicked element has the class "tagChild"
     if (e.target.className === 'tagChild') {
@@ -291,4 +148,4 @@ jobsToDisplay.forEach((job, index) => {
   };
 });
 
-
+// 
